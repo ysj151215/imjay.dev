dda6bc2141f7c88a46a66200efb103325c9bc2bd
Jay Yan <jailjt.editor@gmail.com>
2021-04-14 10:38:14 +0800

    New article & new component (#8)
    * feat: new <MarkdownImage /> component
    
    * fix: update root color
    
    * feat: new article

---
+++ components/MarkdownImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <figure>
+    <img data-zoomable :alt="alt" :src="src" />
+    <figcaption v-if="figcaption" class="text-center">{{ figcaption }}</figcaption>
+  </figure>
+</template>
+
+<script>
+import mediumZoom from 'medium-zoom'
+
+export default {
+  props: {
+    alt: { type: String, default: '' },
+    figcaption: { type: String, default: null },
+    src: { type: String, default: null }
+  },
+  mounted() {
+    mediumZoom('[data-zoomable]', {})
+  }
+}
+</script>
+
+<style>
+.medium-zoom-overlay {
+  background: #fff !important;
+}
+
+html.dark .medium-zoom-overlay {
+  background: #555 !important;
+}
+</style>
