aa0f996973bcb59bc16f005203d806652c2ef05f
ysj151215 <jailjt.editor@gmail.com>
2021-04-22 11:45:37 +0800

    fix: remove focus style

--- components/Header.vue
+++ components/Header.vue
@@ -85,9 +85,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-a {
-  @apply ring-offset-2 focus:no-underline focus:ring-4 focus:outline-none focus:ring-[#fe6958] focus:border-[#fe6958];
-}
-</style>
