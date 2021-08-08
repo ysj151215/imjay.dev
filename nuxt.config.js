1971799fcd7a13d05c40efb33152eb8c4f964eaf
Jay Yan <jailjt.editor@gmail.com>
2021-08-08 10:13:04 +0800

    fix: console warning
    https://github.com/nuxt/nuxt.js/issues/9224

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,5 +53,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]]
+    }
+  }
 }
