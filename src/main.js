a59e9c20ff7dd4a02e0242b97f0ca8a3c4aedc24
Jay Yan <jailjt.editor@gmail.com>
2021-11-01 19:51:20 +0800

    fix: routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { ViteSSG } from 'vite-ssg/single-page'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import Home from './pages/index.vue'
 import 'virtual:windi.css'
 
-export const createApp = ViteSSG(App)
+const routes = [
+  { path: '/:pathMatch(.*)*', redirect: '/' },
+  { path: '/', component: Home }
+]
+
+export const createApp = ViteSSG(App, { routes })
