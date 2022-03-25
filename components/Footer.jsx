6ab00ffbe6a2e75a068b8efa65c804c94e700657
Jay Yan <jailjt.editor@gmail.com>
2022-03-25 15:26:10 +0800

    feat: remove unsplash link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -49,7 +49,6 @@ export default function Footer() {
           <ExternalLink href="https://github.com/ysj151215">GitHub</ExternalLink>
           <ExternalLink href="https://gist.github.com/ysj151215">GitHub Gist</ExternalLink>
           <ExternalLink href="https://twitter.com/ysj151215">Twitter</ExternalLink>
-          <ExternalLink href="https://unsplash.com/@ysj151215">Unsplash</ExternalLink>
         </div>
       </div>
     </footer>
